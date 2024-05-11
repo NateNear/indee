928e2129c6e0352502b63548ec677f3d3573740c
NateNear <askharindrareddy@gmail.com>
2024-05-11 22:42:55 +0530

    accessibility features added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
   <div class="min-h-screen relative">
-    <div class="absolute inset-0 bg-cover" :style="{ 'background-image': `url(${backgroundImage})` }"></div>
-    <div class="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
-    <div class="absolute inset-0 flex items-center justify-center">
-      <LoginPage />
+    <div v-if="isMobile" class="absolute inset-0 flex items-center justify-center">
+      <div class="w-full">
+        <LoginPage />
+      </div>
+    </div>
+    <div>
+      <div
+        class="absolute inset-0 bg-cover"
+        :style="{ 'background-image': `url(${backgroundImage})` }"
+      ></div>
+      <div class="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
+      <div class="absolute inset-0 flex items-center justify-center">
+        <div class="w-full sm:w-96">
+          <LoginPage />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import LoginPage from './components/loginPage.vue';
-import backgroundImage from './assets/netflix_bg.jpg';
+import LoginPage from './components/loginPage.vue'
+import backgroundImage from './assets/netflix_bg.jpg'
 
 export default {
   name: 'App',
   components: {
-    LoginPage,
+    LoginPage
   },
   data() {
     return {
       backgroundImage,
-    };
+      isMobile: false
+    }
   },
-};
-</script>
+  mounted() {
+    this.isMobile = window.innerWidth <= 768
 
-<style scoped>
-/* Add any additional styling if needed */
-</style>
+    window.addEventListener('resize', this.updateIsMobile)
+  },
+  methods: {
+    updateIsMobile() {
+      this.isMobile = window.innerWidth <= 768
+    }
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.updateIsMobile)
+  }
+}
+</script>
--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,71 +1,103 @@
 <template>
   <div class="grid">
-    <div class="bg-black rounded-t-lg p-8 backdrop-filter backdrop-blur-[10px] bg-opacity-80">
-      <div class="flex items-center justify-center mb-6">
-        <img :src="mgmLogo" alt="MGM Global Distribution" class="h-12 filter invert mr-4" />
-        <span class="text-xl text-white">Global<br />Distribution</span>
+    <header
+      class="bg-black rounded-t-lg p-4 backdrop-filter backdrop-blur-[10px] bg-opacity-80"
+      aria-label="Login Form"
+    >
+      <div class="flex items-center justify-center my-3">
+        <img
+          :src="mgmLogo"
+          alt="MGM Global Distribution Logo"
+          class="h-12 filter invert pt-1 mr-2"
+        />
+        <div class="text-md font-semibold grid mb-2 text-white" style="line-height: 1">
+          <p>Global</p>
+          <p>Distribution</p>
+        </div>
       </div>
+
       <div class="mx-6">
-        <h1 class="text-white text-xl font-semibold mb-4 text-left">Sign in</h1>
+        <h1 class="text-white text-xl font-semibold mb-3 text-left">Sign in</h1>
         <form @submit.prevent="handleSubmit">
           <div class="mb-4">
-            <p class="text-[#1A68FF] font-medium">Email:</p>
+            <label for="email" class="text-[#1A68FF] font-medium">Email:</label>
             <input
+              id="email"
               v-model="email"
               type="email"
               placeholder="Enter your email address"
-              class="w-80 px-3 py-2 rounded-sm bg-zinc-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              class="w-full px-3 py-2 rounded-sm bg-zinc-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
+              aria-label="Email Input"
             />
           </div>
           <div class="mb-4">
-            <p class="text-[#1A68FF] font-medium">Password:</p>
+            <label for="password" class="text-[#1A68FF] font-medium">Password:</label>
             <input
+              id="password"
               v-model="password"
               type="password"
               placeholder="Enter your password"
               class="w-full px-3 py-2 rounded-sm bg-zinc-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
+              aria-label="Password Input"
             />
             <a href="#" class="text-[#BCBCBC] text-sm mt-2 block text-right">Forgot Password?</a>
           </div>
-          <div class="flex items-center mb-6">
-            <input v-model="rememberMe" type="checkbox" class="mr-2 w-4 h-4" id="rememberMe" />
-            <label for="rememberMe" class="text-white text-[15px]">Keep me signed in for 30 days</label>
+          <div class="flex items-center mb-4">
+            <input
+              v-model="rememberMe"
+              type="checkbox"
+              class="mr-2 w-4 h-4"
+              id="rememberMe"
+              aria-label="Remember Me Checkbox"
+            />
+            <label for="rememberMe" class="text-white text-[12px]"
+              >Keep me signed in for 30 days</label
+            >
           </div>
           <button
             type="submit"
-            class="w-full py-2 rounded-md bg-[#0057FF] text-white font-bold hover:bg-blue-600 transition-colors duration-300 mx-auto"
+            class="w-full py-2 rounded-md bg-[#0057FF] text-white font-medium hover:bg-blue-600 transition-colors duration-300 mx-auto"
+            :tabindex="rememberMe ? 0 : -1"
+            aria-label="Sign In Button"
           >
             SIGN IN
           </button>
+          <div class="text-white text-start mt-4 text-[12px] flex items-center">
+            <span>Read our Terms of Service</span>
+            <CIcon :icon="cilExternalLink" class="h-3 ml-1" aria-hidden="true" />
+          </div>
         </form>
-        <p class="text-white text-start mt-4 text-[15px]">
-          Read our Terms of Service
+      </div>
+    </header>
+
+    <footer class="bg-[#24272C] rounded-b-lg">
+      <div class="my-auto bg-[#2D3137] h-7"></div>
+      <div class="p-2">
+        <p class="text-[#D5D5D5] text-[8px] text-center">OTHER WAYS TO WATCH</p>
+        <div class="flex items-center justify-center my-3">
+          <img :src="appletvLogo" alt="Apple TV Logo" class="h-4 mr-7 filter invert" />
+          <img :src="iosLogo" alt="iOS Logo" class="h-4 mr-7 filter invert" />
+          <img :src="rokuLogo" alt="Roku Logo" class="h-4 mr-7 filter invert" />
+          <img :src="amazonFireLogo" alt="Amazon Fire Logo" class="h-6 filter invert" />
+        </div>
+        <p class="text-[#D5D5D5] text-[8px] text-center">
+          Search for AppleTV+ FYC on your App Store
         </p>
       </div>
-    </div>
-
-    <!-- New div for "OTHER WAYS TO WATCH" -->
-    <div class="bg-[#24272C] p-4">
-      <p class="text-[#D5D5D5] text-[8px] text-center">OTHER WAYS TO WATCH</p>
-      <div class="flex items-center justify-center my-4">
-        <img :src="appletvLogo" alt="Apple TV" class="h-5 mr-4 filter invert" />
-        <img :src="iosLogo" alt="iOS" class="h-5 mr-4 filter invert" />
-        <img :src="rokuLogo" alt="Roku" class="h-5 mr-4 filter invert" />
-        <img :src="amazonFireLogo" alt="Amazon Fire" class="h-5 filter invert" />
-      </div>
-      <p class="text-[#D5D5D5] text-[8px] text-center">Search for AppleTV+ FYC on your App Store</p>
-    </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import mgmLogo from '../assets/mgm.png';
-import appletvLogo from '../assets/logos/appletv.png';
-import iosLogo from '../assets/logos/ios.png';
-import rokuLogo from '../assets/logos/roku.png';
-import amazonFireLogo from '../assets/logos/firetv.svg';
+import mgmLogo from '../assets/mgm.png'
+import appletvLogo from '../assets/logos/appletv.png'
+import iosLogo from '../assets/logos/ios.png'
+import rokuLogo from '../assets/logos/roku.png'
+import amazonFireLogo from '../assets/logos/firetv.png'
+import { CIcon } from '@coreui/icons-vue'
+import { cilExternalLink } from '@coreui/icons'
 
 export default {
   name: 'LoginPage',
@@ -79,14 +111,18 @@ export default {
       iosLogo,
       rokuLogo,
       amazonFireLogo,
-    };
+      cilExternalLink
+    }
   },
   methods: {
     handleSubmit() {
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
-      console.log('Remember Me:', this.rememberMe);
-    },
+      console.log('Email:', this.email)
+      console.log('Password:', this.password)
+      console.log('Remember Me:', this.rememberMe)
+    }
   },
-};
+  components: {
+    CIcon
+  }
+}
 </script>
